6e53773748967cefd504dc34bf751bf231c4f083
lakshya <lakshya007j@gmail.com>
2025-09-17 18:34:53 +0530

    api

--- APIs/index.py
+++ APIs/index.py
@@ -1,15 +1,12 @@
-# C:\major\Cloud-Burst-Prediction-and-Risk-Analysis\Fuzzy_api.py
-
 from fastapi import FastAPI, Query
 from fastapi.middleware.cors import CORSMiddleware
-from FVI_Fuzzy import FloodVulnerabilityAnalyzer, district_to_coords
+from APIs.FVI_Fuzzy import FloodVulnerabilityAnalyzer, district_to_coords
 
 app = FastAPI()
 
-# Enable CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -19,14 +16,9 @@ analyzer = FloodVulnerabilityAnalyzer(cache_enabled=True)
 
 @app.get("/fvi")
 def get_fvi(lat: float = Query(...), lon: float = Query(...), district: str = None):
-    """
-    Returns Flood Vulnerability Index for given lat/lon or district
-    """
     district_info = None
     if district:
         coords = district_to_coords(district)
         if coords:
             lat, lon, district_info = coords
-
-    result = analyzer.calculate_fvi(lat, lon, district_info=district_info)
-    return result
+    return analyzer.calculate_fvi(lat, lon, district_info=district_info)
